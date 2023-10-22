c144c70aae3086b2ad398d6ee6133a4361632280
Razmik Poghosyan <razmikarm@gmail.com>
2023-10-22 07:27:33 +0400

    Separated Validator from Controller

--- core/controller.py
+++ core/controller.py
@@ -1,13 +1,12 @@
 from uuid import uuid4, UUID
 
 from .board import Board
-from .utils import Colors
+from .utils import Colors, Validator
 
 class Controller:
 
     def __init__(self):
         self.boards = {}
-        self._letters = ' ABCDEFGH'
 
     def make_move(self, board_id, from_cell, to_cell):
         board_id = self.convert_id(board_id)
@@ -15,13 +14,18 @@ class Controller:
             return
         if not (board := self.boards.get(board_id)):
             return
-        if not self.is_valid_cellname(from_cell):
+        if not Validator.is_valid_cellname(from_cell):
             return
-        if not self.is_valid_cellname(to_cell):
+        if not Validator.is_valid_cellname(to_cell):
             return
-        from_pos = self.cellname_to_pos(from_cell)
-        to_pos = self.cellname_to_pos(to_cell)
-        return board.make_move(from_pos, to_pos)
+        from_pos = Validator.cellname_to_pos(from_cell)
+        to_pos = Validator.cellname_to_pos(to_cell)
+        state = board.make_move(from_pos, to_pos)
+        if board.loser:
+            self.game_over(board)
+
+    def game_over(self, board):
+        pass # TODO: Implement actions when the game is over | loser can be accessed by board.loser
 
     def start_new_board(self):
         new_id = uuid4()
@@ -46,92 +50,3 @@ class Controller:
         if not self.boards.get(board_id):
             return
         return self.boards[board_id].state
-
-    def is_valid_cellname(self, cellname):
-        if not isinstance(cellname, str):
-            return False
-        if len(cellname) != 2:
-            return False
-        if not self.is_valid_column(cellname[0]):
-            return False
-        if not self.is_valid_row(cellname[1]):
-            return False
-        return True
-
-    def is_valid_pos(self, pos):
-        if not isinstance(pos, (tuple, list)):
-            return False
-        if len(pos) != 2:
-            return False
-        if not (0 <= pos[0] <= 7):
-            return False
-        if not (0 <= pos[1] <= 7):
-            return False
-        return True
-
-    def is_valid_row(self, row):
-        if not isinstance(row, (int, str)):
-            return False
-        if isinstance(row, str):
-            if not (len(row) == 1 and row.isdigit()):
-                return False
-            row = int(row)
-        if not (0 < row < 9):
-            return False
-        return True
-
-    def is_valid_column(self, column):
-        if not isinstance(column, (int, str)):
-            return False
-        if isinstance(column, str):
-            if not (len(column) == 1 and column != ' '):
-                return False
-            return column in self._letters
-        if not (0 < column < 9):
-            return False 
-        return True
-    
-    def convert_id(self, value):
-        if isinstance(value, UUID):
-            return value
-        if not isinstance(value, str):
-            return
-        try:
-            return UUID(value)
-        except ValueError:
-            return
-    
-    def convert_column(self, column):
-        if not self.is_valid_column(column_number):
-            return None
-        if isinstance(column, str):
-            return self._letters.find(column)
-        return self._letters[column]
-
-    def cellname_to_pos(self, cellname):
-        if not self.is_valid_cellname(cellname):
-            return None
-        column = self.convert_column(cellname[0])
-        row = int(cellname[1])
-
-        real_column = column - 1
-        real_row = 8 - row
-
-        return real_row, real_column
-
-    def pos_to_cellname(self, pos):
-        if not self.is_valid_pos(pos):
-            return None        
-        real_row, real_column = pos
-
-        row = 8 - real_row
-        column = real_column + 1
-        column = self.convert_column(column)
-
-        return f"{column}{row}"
-
-    def get_cell_color(self, cellname):
-        pos = self.cellname_to_pos(cellname)
-        if not pos:
-            return None
-        return sum(pos) % 2
--- core/utils.py
+++ core/utils.py
@@ -3,7 +3,111 @@ class Colors:
     WHITE = 'White'
     BLACK = 'Black'
 
+
 ColorsDict = {
     0: Colors.WHITE,
     1: Colors.BLACK,
 }
+
+
+class Validator:
+
+    _letters = ' ABCDEFGH'
+
+    @classmethod
+    def is_valid_cellname(cls, cellname):
+        if not isinstance(cellname, str):
+            return False
+        if len(cellname) != 2:
+            return False
+        if not cls.is_valid_column(cellname[0]):
+            return False
+        if not cls.is_valid_row(cellname[1]):
+            return False
+        return True
+
+    @classmethod
+    def is_valid_pos(cls, pos):
+        if not isinstance(pos, (tuple, list)):
+            return False
+        if len(pos) != 2:
+            return False
+        if not (0 <= pos[0] <= 7):
+            return False
+        if not (0 <= pos[1] <= 7):
+            return False
+        return True
+
+    @classmethod
+    def is_valid_row(cls, row):
+        if not isinstance(row, (int, str)):
+            return False
+        if isinstance(row, str):
+            if not (len(row) == 1 and row.isdigit()):
+                return False
+            row = int(row)
+        if not (0 < row < 9):
+            return False
+        return True
+
+    @classmethod
+    def is_valid_column(cls, column):
+        if not isinstance(column, (int, str)):
+            return False
+        if isinstance(column, str):
+            if not (len(column) == 1 and column != ' '):
+                return False
+            return column in cls._letters
+        if not (0 < column < 9):
+            return False 
+        return True
+    
+    @classmethod
+    def convert_id(cls, value):
+        if isinstance(value, UUID):
+            return value
+        if not isinstance(value, str):
+            return
+        try:
+            return UUID(value)
+        except ValueError:
+            return
+    
+    @classmethod
+    def convert_column(cls, column):
+        if not cls.is_valid_column(column_number):
+            return None
+        if isinstance(column, str):
+            return cls._letters.find(column)
+        return cls._letters[column]
+
+    @classmethod
+    def cellname_to_pos(cls, cellname):
+        if not cls.is_valid_cellname(cellname):
+            return None
+        column = cls.convert_column(cellname[0])
+        row = int(cellname[1])
+
+        real_column = column - 1
+        real_row = 8 - row
+
+        return real_row, real_column
+
+    @classmethod
+    def pos_to_cellname(cls, pos):
+        if not cls.is_valid_pos(pos):
+            return None        
+        real_row, real_column = pos
+
+        row = 8 - real_row
+        column = real_column + 1
+        column = cls.convert_column(column)
+
+        return f"{column}{row}"
+
+    @classmethod
+    def get_cell_color(cls, cellname):
+        pos = cls.cellname_to_pos(cellname)
+        if not pos:
+            return None
+        return sum(pos) % 2
